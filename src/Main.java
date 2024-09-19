cd5457133ea7aae6751c5cefa19ed3588daedf99
Mujtaba-abd <146767335+Mujtaba-abd@users.noreply.github.com>
2024-09-19 18:25:21 +0300

    added on click event to change the text on the button from empty to X or O and added turns to know what to change the text to x or o made the text biger (x, o)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.awt.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import java.awt.Toolkit;
-import java.awt.Dimension;
-import java.awt.GridLayout;
 
 public class Main {
+
   public static void main(String[] args) {
     JFrame Frame = new JFrame();
     Frame.setSize(800, 600);
@@ -34,6 +37,8 @@ public class Main {
     JButton b8 = new JButton("");
     JButton b9 = new JButton("");
 
+    JButton[] buttons = { b1, b2, b3, b4, b5, b6, b7, b8, b9 };
+
     Frame.add(b1);
     Frame.add(b2);
     Frame.add(b3);
@@ -43,5 +48,52 @@ public class Main {
     Frame.add(b7);
     Frame.add(b8);
     Frame.add(b9);
+
+    ActionListener buttonListener = new ActionListener() {
+      int counter = 0;
+
+      public void actionPerformed(ActionEvent ae) {
+        JButton button = (JButton) ae.getSource();
+        if (counter == 0) {
+          button.setText("X");
+        }
+        if (counter == 1) {
+          button.setText("O");
+        }
+        if (counter == 2) {
+          button.setText("X");
+        }
+        if (counter == 3) {
+          button.setText("O");
+        }
+        if (counter == 4) {
+          button.setText("X");
+        }
+        if (counter == 5) {
+          button.setText("O");
+        }
+        if (counter == 6) {
+          button.setText("X");
+        }
+        if (counter == 7) {
+          button.setText("O");
+        }
+        if (counter == 8) {
+          button.setText("X");
+        }
+        counter++;
+      }
+    };
+    for (JButton button : buttons) {
+      button.setFont(new Font("Arial", Font.BOLD, 25));
+    }
+    for (JButton button : buttons) {
+      button.addActionListener(buttonListener);
+    }
+
+    if (b1.getText().equals(b2.getText())) {
+      System.out.println("boy");
+
+    }
   }
 }
